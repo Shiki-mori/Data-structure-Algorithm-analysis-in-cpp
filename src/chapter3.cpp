d4b91556fbb0372251e568386afdb471b76d2b12
Shiki-mori <2296690003@qq.com>
2026-01-18 14:23:49 +0800

    until 3.3.2

---
+++ src/chapter3.cpp
@@ -0,0 +1,15 @@
+// for (int i = 0; i != v.size(); i++)
+//   cout << v[i] << endl;
+
+// for (vector<int>::iterator itr = v.begin(); itr != v.end(); itr++)
+//   cout << *itr << endl;
+
+// vector<int>::iterator itr = v.begin();
+// while (itr != v.end())
+//   cout << *itr++ << endl;
+
+// iterator insert(iterator pos, const Object &x);
+
+// iterator erase(iterator pos);
+
+// iterator erase(iterator start, iterater end);
