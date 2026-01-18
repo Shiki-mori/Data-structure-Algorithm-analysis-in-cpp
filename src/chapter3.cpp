36a63c1d13df376c1d5d4954e2c9a9169a9d932e
Shiki-mori <2296690003@qq.com>
2026-01-18 19:21:02 +0800

    completed 3.3

--- src/chapter3.cpp
+++ src/chapter3.cpp
@@ -13,3 +13,56 @@
 // iterator erase(iterator pos);
 
 // iterator erase(iterator start, iterater end);
+
+// #include <iostream>
+// #include <list>
+// #include <vector>
+// using namespace std;
+// // 3.5 使用迭代器隔项删除表中的项
+// template <typename Container> void removeEveryOtherItem(Container &lst) {
+//   auto itr = lst.begin();
+
+//   while (itr != lst.end()) {
+//     itr = lst.erase(itr);
+//     if (itr != lst.end())
+//       ++itr;
+//   }
+// }
+
+// template <typename Container, typename Object>
+// void change(Container &c, const Object &newValue) {
+//   // Container::iterator itr = c.begin();
+//   typename Container::iterator itr = c.begin();
+//   // auto itr = c.begin();
+//   while (itr != c.end()) {
+//     *itr++ = newValue;
+//   }
+// }
+
+// // 3.6打印任意容器
+// template <typename Container>
+// void print(const Container &c, ostream &out = cout) {
+//   if (c.empty())
+//     out << "(empty)";
+//   else {
+//     auto itr = begin(c);
+//     out << "[" << *itr++;
+
+//     while (itr != end(c))
+//       out << "," << *itr++;
+
+//     out << "]" << endl;
+//   }
+// }
+
+// int main() {
+//   list<int> List = {0, 1, 2, 3, 4};
+//   print(List);
+//   removeEveryOtherItem(List);
+//   print(List);
+//   change(List, 100);
+//   auto listpos = List.begin();
+//   print(List);
+
+//   return 0;
+// }
--- src/tempCodeRunnerFile.cpp
+++ src/tempCodeRunnerFile.cpp
@@ -1,6 +1,5 @@
- cout << binarySearch(nums1, 0) << endl;
-  cout << binarySearch(nums1, 1) << endl;
-  cout << binarySearch(nums1, 2) << endl;
-  cout << binarySearch(nums1, 3) << endl;
-  cout << binarySearch(nums1, 4) << endl;
-  cout << binarySearch(nums1, 5) << endl;
+  for (auto listpos = List.begin(); listpos != List.end(); listpos++) {
+    while (listpos != List.end()) {
+      cout << *listpos++ << endl;
+    }
+  }
